a11c208140084235caad795ef980745591d58c7a
4qwee <tmp@email.ru>
2012-08-14 00:55:23 +0300

    buttons

--- public/site_media/js/myscript.js
+++ public/site_media/js/myscript.js
@@ -10,6 +10,8 @@ myJS = function ()
     $("#positions-table tbody tr").live('click', function (e)
     {
         var myTable = $('#positions-table');
+        var toolbar = $("div.toolbar");
+
 
         if ($(this).hasClass('row_selected'))
         {
@@ -17,8 +19,22 @@ myJS = function ()
         }
         else
         {
-            myTable.find('.row_selected').removeClass('row_selected');
+            myTable.find('tr.row_selected').removeClass('row_selected');
             $(this).addClass('row_selected');
+
+            toolbar.empty();
+
+            $sale = $('<input/>').attr('type', 'button').val('Продажа').addClass('myButton');
+            toolbar.append($sale);
+
+            $expense = $('<input/>').attr('type', 'button').val('Расход').addClass('myButton');
+            toolbar.append($expense);
+
+            $hands = $('<input/>').attr('type', 'button').val('На руки').addClass('myButton');
+            toolbar.append($hands);
+
+            $order = $('<input/>').attr('type', 'button').val('Заказ').addClass('myButton');
+            toolbar.append($order);
         }
     });
 };
