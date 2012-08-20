d22921ee80568523e7f30e7681d82a8365f4c8c0
4qwee <tmp@email.ru>
2012-08-20 23:03:27 +0300

    markets

--- mazdai_app/admin.py
+++ mazdai_app/admin.py
@@ -1,11 +1,23 @@
+# coding=utf-8
 __author__ = '4qwee'
 
 from django.contrib import admin
-from mazdai_app.models import Position
+from mazdai_app.models import *
+
+class GoodsQuantityInline(admin.StackedInline):
+    model = GoodsQuantity
+
+    verbose_name = 'Магазин'
 
 class PositionAdmin(admin.ModelAdmin):
     list_display = ('id', 'name')
     list_editable = ('name',)
     ordering = ['id']
+    inlines = [GoodsQuantityInline]
+
+class MarketAdmin(admin.ModelAdmin):
+    list_display = ('name', )
+    ordering = ['name']
 
 admin.site.register(Position, PositionAdmin)
+admin.site.register(Market, MarketAdmin)
--- mazdai_app/models.py
+++ mazdai_app/models.py
@@ -4,12 +4,40 @@ from django.db import models
 class Position(models.Model):
 
     name = models.CharField(max_length=300, verbose_name='Имя')
-    quantity = models.IntegerField(verbose_name='Количество')
     price = models.FloatField(verbose_name='Цена')
     description = models.TextField(blank=True, verbose_name='Описание')
 
-class SaleEntry(models.Model):
+    def __unicode__(self):
+        return self.name
 
+    class Meta:
+        verbose_name = 'Позиция'
+        verbose_name_plural = 'Позиции'
+
+class Market(models.Model):
+    name = models.CharField(max_length=250, verbose_name='Имя')
+    description = models.TextField(blank=True, verbose_name='Описание')
+
+    def __unicode__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Магазин'
+        verbose_name_plural = 'Магазины'
+
+class GoodsQuantity(models.Model):
+    position = models.ForeignKey(Position, verbose_name='Позиция')
+    market = models.ForeignKey(Market, verbose_name='Магазин')
+    quantity = models.IntegerField(verbose_name='Количество')
+
+    def __unicode__(self):
+        return '%s(%s) - %d' % (self.position, self.market.name, self.quantity)
+
+    class Meta:
+        verbose_name = 'Количество товара'
+        verbose_name_plural = 'Количества товара'
+
+class SaleEntry(models.Model):
     position = models.ForeignKey(Position)
     date = models.DateTimeField(verbose_name='Дата')
     quantity = models.IntegerField(verbose_name='Количество', max_length=3)
