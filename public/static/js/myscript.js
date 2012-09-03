25e1b06702547f5cfbeff7462ff50b6cae379e57
4qwee <tmp@email.ru>
2012-09-04 00:04:29 +0300

    refactoring & some small bugs

--- public/static/js/myscript.js
+++ public/static/js/myscript.js
@@ -7,97 +7,115 @@
 
 myJS = function ()
 {
-    $("#positions-table tbody tr").live('click', function (e)
+    function createPopup($sales_popup, title)
     {
-        var myTable = $('#positions-table');
-        var $toolbar = $("div.toolbar");
-        var $this = $(this);
-
-        var $sales_popup = $('#salesPopup');
-        $sales_popup.find('p').first().hide();
         $sales_popup.dialog({
             modal:true,
-            title:'Продажа',
+            title:title,
             show:{effect:'puff', duration:100},
             hide:{effect:'puff', duration:100},
-            autoOpen: false
+            autoOpen:false
         });
+    }
 
-        if ($this.find('td').first().hasClass('dataTables_empty'))
+    function createActionButton($row, $popup, label)
+    {
+        var $sale = $('<input/>').attr('type', 'button').val(label).addClass('myButton');
+        $sale.click(function (e)
+        {
+            var number = $row.find('td').first().html();
+            $popup.find('#id_position_id').val(number);
+            $popup.dialog('open');
+        });
+        return $sale;
+    }
+
+    function createActionBar($row, $toolbar)
+    {
+        $toolbar.append(createActionButton($row, $sales_popup, 'Продажа'));
+
+        var $expense = $('<input/>').attr('type', 'button').val('Расход').addClass('myButton');
+        $toolbar.append($expense);
+
+        var $hands = $('<input/>').attr('type', 'button').val('На руки').addClass('myButton');
+        $toolbar.append($hands);
+
+        var $order = $('<input/>').attr('type', 'button').val('Заказ').addClass('myButton');
+        $toolbar.append($order);
+
+        $toolbar.buttonset();
+    }
+
+    function createSubmitHandler($table, $popup, $toolbar)
+    {
+        return function (e)
+        {
+            e.preventDefault();
+
+            var $form = $popup.find('form');
+            var $errors = $popup.find('.errors');
+
+            $form.find('.submit_button').attr('disabled', 'disabled');
+            $form.find('.progress_image').show();
+
+            $.ajax({
+                url:$form.attr('action'),
+                type:$form.attr('method'),
+                data:$form.serialize(),
+                dataType:'json',
+                success:function (json)
+                {
+                    $form.find('img.progress_image').hide();
+                    $popup.find('.submit_button').removeAttr('disabled');
+                    if (json.success)
+                    {
+                        $popup.dialog('close');
+                        $toolbar.empty();
+                        $table.dataTable().fnClearTable();
+                        $table.dataTable().fnDraw();
+                    }
+                    else
+                        $errors.noty({text:json.html, type:'error'});
+                },
+                error:function (xhr, ajaxOptions, thrownError)
+                {
+                    $errors.noty({text:thrownError, type:'error'});
+                }
+            })
+        };
+    }
+
+    var $myTable = $('#positions-table');
+    var $toolbar = $("div.toolbar");
+    var $sales_popup = $('#salesPopup');
+
+    $sales_popup.find('p').first().hide();// hide first column
+
+    createPopup($sales_popup, 'Продажа');
+
+    $('#bottom_links').buttonset();
+
+    $("#positions-table tbody tr").live('click', function (e)
+    {
+        var $this = $(this);
+
+        if ($this.find('td').first().hasClass('dataTables_empty')) //don't select header
             return;
 
         if ($this.hasClass('row_selected'))
         {
             $this.removeClass('row_selected');
-
             $toolbar.empty();
         }
         else
         {
-            myTable.find('tr.row_selected').removeClass('row_selected');
+            $myTable.find('tr.row_selected').removeClass('row_selected');
             $this.addClass('row_selected');
-
             $toolbar.empty();
 
-            $sale = $('<input/>').attr('type', 'button').val('Продажа').addClass('myButton');
-            $sale.click(function(e)
-            {
-                var number = $this.find('td').first().html();
-                $sales_popup.find('#id_position_id').val(number);
-                $sales_popup.dialog('open');
-            });
-            $toolbar.append($sale);
-
-            $expense = $('<input/>').attr('type', 'button').val('Расход').addClass('myButton');
-            $toolbar.append($expense);
-
-            $hands = $('<input/>').attr('type', 'button').val('На руки').addClass('myButton');
-            $toolbar.append($hands);
-
-            $order = $('<input/>').attr('type', 'button').val('Заказ').addClass('myButton');
-            $toolbar.append($order);
-
-            $toolbar.buttonset();
+            createActionBar($this, $toolbar);
         }
     });
 
-    var $sales_link = $('#bottom_links');
-    $sales_link.buttonset();
-
-    var $sales_form = $('#sales_form');
-    $sales_form.submit(function(e)
-    {
-        e.preventDefault();
-
-        $sales_form.find('.submit_button').attr('disabled', 'disabled');
-        $("div.toolbar").empty();
-        $sales_form.find('.progress_image').show();
-
-        $.ajax({
-            url:$sales_form.attr('action'),
-            type:$sales_form.attr('method'),
-            data:$sales_form.serialize(),
-            dataType:'json',
-            success:function(json)
-            {
-                $sales_form.find('img.progress_image').hide();
-                $('#salesPopup').find('.submit_button').removeAttr('disabled');
-
-                if (json.success)
-                {
-                    $('#salesPopup').dialog('close');
-
-                    var $table = $('#positions-table');
-                    $table.dataTable().fnClearTable();
-                    $table.dataTable().fnDraw();
-                }
-                else
-                    $('#sales_error').noty({text: json.html, type: 'error'});
-            },
-            error:function(xhr, ajaxOptions, thrownError)
-            {
-                $('#sales_error').noty({text: thrownError, type: 'error'});
-            }
-        })
-    });
+    $sales_popup.find('form').submit(createSubmitHandler($myTable, $sales_popup, $toolbar));
 };
